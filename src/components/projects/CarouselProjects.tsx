0643b887883bc40792b229ceb05d5271d13746bd
charlieboudou <charlie.lcb3@gmail.com>
2024-07-08 11:40:08 +0400

    add nav when mobile for projects caroussel

--- src/components/projects/CarouselProjects.tsx
+++ src/components/projects/CarouselProjects.tsx
@@ -2,12 +2,15 @@ import Carousel from "react-spring-3d-carousel";
 import React, { useState } from "react";
 import { config } from "react-spring";
 import { IProjectCard } from "../../utils/types";
+import { useSelector } from "react-redux";
 
 interface ICarouselProjectsProps {
     cards: IProjectCard[],
 }
 
 function CarouselProjects({ cards }: ICarouselProjectsProps) {
+    const theme = useSelector((state: any) => state.theme);
+    
     const table = cards.map((element: IProjectCard, index: number) => {
         return { ...element, onClick: () => setGoToSlide(index) };
     });
@@ -16,7 +19,7 @@ function CarouselProjects({ cards }: ICarouselProjectsProps) {
     const [card] = useState(table);
 
     return (
-        <div className="w-full md:p-[3rem] h-screen flex items-center">
+        <div className="w-full md:p-[3rem] h-screen flex flex-col items-center justify-center relative">
             <div
                 className={`md:w-[80%] mx-auto w-full`}
             >
@@ -24,9 +27,19 @@ function CarouselProjects({ cards }: ICarouselProjectsProps) {
                     slides={card}
                     goToSlide={goToSlide}
                     offsetRadius={1}
-                    showNavigation={true}
+                    showNavigation={false}
                     animationConfig={config.gentle}
                 />
+            </div>
+            <div className="block md:hidden absolute bottom-[12rem] flex space-x-[.5rem]">
+                {cards.map((_, index) => (
+                    <button
+                        key={index}
+                        onClick={() => setGoToSlide(index)}
+                        className="p-[.5rem] rounded-full transition-colors duration-300"
+                        style={{ backgroundColor: `${goToSlide === index ? `${theme.icon}` : 'rgb(156 163 175)'}`}}
+                    />
+                ))}
             </div>
         </div>
     );
