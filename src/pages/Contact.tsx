69b3f1dd96bf4c964800b331d29730a2aecd7de4
charlieboudou <charlie.lcb3@gmail.com>
2024-07-10 19:43:56 +0400

    add background contact

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -10,15 +10,15 @@ function Contact(): JSX.Element {
   const why = about(t).find((element) => element.name === "why");
 
   return (
-    <div className={`p-[1rem] h-screen md:h-full w-full flex flex-col items-center`}>
+    <div className={`p-[1rem] h-screen md:h-full w-full flex flex-col items-center ${theme.isToggle ? "bg-[url('../../public/images/homeBackgroundPurple.png')]" : "bg-[url('../../public/images/homeBackgroundBlue.png')]"} bg-no-repeat bg-auto md:bg-contain md:bg-right bg-center`}>
       <div
         className={`md:px-[3rem] md:py-[3rem] pb-[1rem] w-[90%] m-auto mb-[2rem] text-center border-[.2rem] rounded-[1rem]`}
         style={{borderColor: `${theme.icon}`}}
       >
-        <div className="md:text-[2rem] md:w-[90%] md:m-auto text-justify p-[1rem]" style={{ color: `${theme.activeIcon}`}}>
+        <div className="md:text-[2rem] md:w-[90%] md:m-auto text-justify p-[1rem] font-bold" style={{ color: `${theme.activeIcon}`}}>
           {why && why.description}
         </div>
-        <p className="md:text-[5rem] text-[2rem]" style={{ color: `${theme.textContact}`}}>{t('youLike')}</p>
+        <p className="md:text-[5rem] text-[2rem] font-bold" style={{ color: `${theme.textContact}`}}>{t('youLike')}</p>
         <p className="md:text-[3rem] text-[1.8rem] pb-[.5rem]" style={{ color: `${theme.activeIcon}`}}>{t('contactMe')}</p>
         <a 
           href="mailto:charlie.lcb3@gmail.com"
